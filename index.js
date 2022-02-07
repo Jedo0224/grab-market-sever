edb0a7fe17993800efaa269c9388cc40cbc0d568
unknown <>
2022-02-07 18:15:18 +0900

    heroku 배포 준비

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const upload = multer({
     },
   }),
 });
-const port.process.env.PORT || 8080;
+const port = process.env.PORT || 8080;
 app.use(express.json());
 app.use(cors());
 app.use("/uploads", express.static("uploads"));
